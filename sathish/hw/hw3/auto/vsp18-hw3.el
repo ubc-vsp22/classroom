5bab858368450aabd1d83e56014ee8c4131da190
Sathish Gopalakrishnan <sathish.gopalakrishnan@gmail.com>
2022-07-26 11:03:44 -0700

    added divide & conquer sldiquer slides

---
+++ sathish/hw/hw3/auto/vsp18-hw3.el
@@ -0,0 +1,62 @@
+(TeX-add-style-hook
+ "vsp18-hw3"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1") ("euscript" "mathscr")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "latexsym"
+    "algorithm"
+    "algorithmic"
+    "graphicx"
+    "subfigure"
+    "fontenc"
+    "newcent"
+    "amsmath"
+    "amssymb"
+    "amsfonts"
+    "fullpage"
+    "hyphenat"
+    "multirow"
+    "enumitem"
+    "euscript")
+   (TeX-add-symbols
+    "E"
+    "Q"
+    "R"
+    "Z"
+    "N"
+    "C"
+    "K"
+    "cA"
+    "cF"
+    "cB"
+    "cM"
+    "cG"
+    "cP"
+    "cL"
+    "cX"
+    "cZ"
+    "cE"
+    "cN"
+    "cT"
+    "ran"
+    "dom"
+    "supp"
+    "eps"
+    "var"
+    "ind")
+   (LaTeX-add-environments
+    "theorem"
+    "lemma"
+    "corollary"
+    "definition"
+    "property"
+    "observation"
+    "remark"
+    "proof")))
+
